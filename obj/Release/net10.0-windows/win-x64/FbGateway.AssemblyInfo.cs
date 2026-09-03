61e468443c939f00ce0ed05bc9b15c69784aefb3
KenanWahbeh <kenanwahbeh@my.uopeople.edu>
2026-09-03 06:43:16 +0300

    add

--- obj/Release/net10.0-windows/win-x64/FbGateway.AssemblyInfo.cs
+++ obj/Release/net10.0-windows/win-x64/FbGateway.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FbGateway")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b88f7d16144eb7aa68aad9871105357d683835bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("FbGateway")]
